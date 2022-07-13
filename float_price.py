44cefd02b6c681df1647817eaf23c0865d8333da
Prince Idris <gualdjouma@aim.com>
2022-07-13 14:43:59 -0400

    Create float_price.py
    for day traders who mainly trade low floaters

---
+++ float_price.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+from time import time
+
+symbol = input('Enter ticker symbol:')
+
+start = time()
+
+try:
+	ticker = yf.Ticker(symbol)
+
+	share_float = str('float = M'), round(ticker.info['floatShares'] / 1000000, 1)
+	price = str('price = $'), round(ticker.info['currentPrice'] / 1, 10)
+
+	df = pd.DataFrame([share_float, price])
+	print(df)
+except:
+    print('No data returned for this ticker')
+
+end = time()
+print('Time taken: ' + str(round(end - start, 2)) + 's')
